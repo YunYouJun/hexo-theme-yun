5cb517e0aa7f20b51eb5abf45c492da74fdefe1a
云游君 <me@yunyoujun.cn>
2019-04-11 22:47:16 +0800

    generate template

---
+++ README.md
@@ -0,0 +1,22 @@
+# Hexo-Theme-Starry
+
+Starry sky.
+
+## Install
+
+```sh
+git clone -b master https://github.com/Molunerfinn/hexo-theme-melody themes/melody
+```
+
+## About
+
+- [Hexo](https://hexo.io)
+
+## Ref
+
+- [Hexo主题开发经验杂谈](https://molunerfinn.com/make-a-hexo-theme/)
+
+## Inspiration
+
+- [【洛天依原创】异样的风暴中心【杉田朗】](https://www.bilibili.com/video/av4018008)
+- [【洛天依原创】星宿计时【杉田朗】](https://www.bilibili.com/video/av7036967)
---
+++ scripts/replace-config.js
@@ -0,0 +1,18 @@
+/**
+ * Note: configs in _data/starry.yml will replace configs in hexo.theme.config.
+ */
+
+const version = require('../package.json').version
+
+hexo.extend.helper.register('version', function () {
+  return version
+})
+
+hexo.on('generateBefore', function () {
+  const rootConfig = hexo.config
+  if (hexo.locals.get) {
+    const data = hexo.locals.get('data')
+    data && data.starry && (hexo.theme.config = data.starry)
+  }
+  hexo.theme.config.rootConfig = rootConfig
+})
