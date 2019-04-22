a249cd10bf66af2ecc37c72c5cb68e2f50c670a2
云游君 <me@yunyoujun.cn>
2019-04-22 16:51:12 +0800

    add custom firework

--- source/js/utils.js
+++ source/js/utils.js
@@ -1,3 +1,38 @@
+function debounce(func, wait, immediate) {
+  var timeout
+  return function () {
+    var context = this
+    var args = arguments
+    var later = function () {
+      timeout = null
+      if (!immediate) func.apply(context, args)
+    }
+    var callNow = immediate && !timeout
+    clearTimeout(timeout)
+    timeout = setTimeout(later, wait)
+    if (callNow) func.apply(context, args)
+  }
+}
+
+function throttle(func, wait, mustRun) {
+  var timeout
+  var startTime = new Date()
+
+  return function () {
+    var context = this
+    var args = arguments
+    var curTime = new Date()
+
+    clearTimeout(timeout)
+    if (curTime - startTime >= mustRun) {
+      func.apply(context, args)
+      startTime = curTime
+    } else {
+      timeout = setTimeout(func, wait)
+    }
+  }
+}
+
 Starry.utils = {
   /**
    * Transform embedded video to support responsive layout.
@@ -78,3 +113,6 @@ Starry.utils = {
     });
   }
 }
+
+window.debounce = debounce
+window.throttle = throttle
