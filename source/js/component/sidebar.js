62662e824680561a6ca9c414bebde68924b540ef
云游君 <me@yunyoujun.cn>
2019-04-24 14:46:21 +0800

    fix body padding when open sidebar

--- source/js/component/sidebar.js
+++ source/js/component/sidebar.js
@@ -1,82 +1,35 @@
+const sidebarWidth = '320px'
 $(function () {
   $('.hamburger').on('click', function() {
     let isOpen = $(this).hasClass('is-active')
     isOpen ? $(this).removeClass('is-active') : $(this).addClass('is-active')
-    // var toggleText = $(this).attr('data-toggle')
-    // $(this).attr('data-toggle', $(this).text())
-    // $(this).text(toggleText)
-    // changeSideBarInfo()
+    let isSmall = $(window).width() < 768
     if (isOpen) {
       $('#sidebar').velocity('stop').velocity({
         translateX: '0px'
       }, {
         duration: 200
       })
+      if (!isSmall) {
+        $('body').velocity('stop').velocity({
+          paddingLeft: '0px'
+        }, {
+          duration: 200
+        })
+      }
     } else {
       $('#sidebar').velocity('stop').velocity({
-        translateX: '320px'
+        translateX: sidebarWidth
       }, {
         duration: 200
       })
-    }
-  })
-  $('.text').on('click', function () {
-    if (!isMobile() && $('#sidebar').is(':visible')) {
-      
-      if (isOpen) {
-        $('#page-header').removeClass('open-sidebar')
+      if (!isSmall) {
         $('body').velocity('stop').velocity({
-          paddingLeft: '300px'
-        }, {
-          duration: 200
-        })
-        
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '0deg',
-          color: '#1F2D3D'
-        }, {
-          duration: 200
-        })
-      } else {
-        $('#page-header').addClass('open-sidebar')
-        $('body').velocity('stop').velocity({
-          paddingLeft: '300px'
-        }, {
-          duration: 200
-        })
-        $('#sidebar').velocity('stop').velocity({
-          translateX: '300px'
-        }, {
-          duration: 200
-        })
-        $('#toggle-sidebar').velocity('stop').velocity({
-          rotateZ: '180deg',
-          color: '#99a9bf'
+          paddingLeft: sidebarWidth
         }, {
           duration: 200
         })
       }
     }
   })
-  function changeSideBarInfo () {
-    if ($('.author-info').is(':visible')) {
-      $('.author-info').velocity('stop')
-        .velocity('transition.slideLeftOut', {
-          duration: 300,
-          complete: function () {
-            $('.sidebar-toc').velocity('stop')
-              .velocity('transition.slideRightIn', { duration: 500 })
-          }
-        })
-    } else {
-      $('.sidebar-toc').velocity('stop')
-        .velocity('transition.slideRightOut', {
-          duration: 300,
-          complete: function () {
-            $('.author-info').velocity('stop')
-              .velocity('transition.slideLeftIn', { duration: 500 })
-          }
-        })
-    }
-  }
 })
