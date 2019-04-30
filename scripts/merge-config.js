49fa7039094ab02b2f8dd18303be3f794e86f71d
YunYouJun <me@yunyoujun.cn>
2019-04-30 23:51:50 +0800

    add gitalk post-nav

---
+++ scripts/merge-config.js
@@ -0,0 +1,19 @@
+/**
+ * Note: configs.xxx in _data/starry.yml will replace configs in hexo.theme.config.xxx.
+ */
+
+const version = require('../package.json').version
+
+hexo.extend.helper.register('version', function () {
+  return version
+})
+
+hexo.on('generateBefore', function () {
+  if (hexo.locals.get) {
+    const data = hexo.locals.get('data')
+    if (hexo.config && hexo.theme.config && data.starry) {
+      hexo.config = Object.assign(hexo.config, data.starry)
+      hexo.theme.config = Object.assign(hexo.theme.config, data.starry)
+    }
+  }
+})
