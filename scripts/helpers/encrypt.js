f852bd2b85da1dc86fa7106b70731b3ae2ceb19f
YunYouJun <me@yunyoujun.cn>
2020-04-28 12:31:29 +0800

    :construction_worker: try trigger netlify

--- scripts/helpers/encrypt.js
+++ scripts/helpers/encrypt.js
@@ -1,5 +1,4 @@
-const CryptoJS = require("crypto-js");
-
 hexo.extend.helper.register("aes", function(content, password) {
+  const CryptoJS = require("crypto-js");
   return CryptoJS.AES.encrypt(content, password).toString();
 });
---
+++ trigger.sh
@@ -0,0 +1,1 @@
+curl -X POST -d '{"event_type": "on-demand-test"}' -H "Content-Type: application/json" https://api.github.com/repos/YunYouJun/yunyoujun.github.io/dispatches
