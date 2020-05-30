b9f30863c09443698fd8f265e7579f366ae9c2cd
YunYouJun <me@yunyoujun.cn>
2020-05-30 23:19:47 +0800

    :zap: use IntersectionObserver rewrite sidebar toc

--- source/js/sidebar.js
+++ source/js/sidebar.js
@@ -1,23 +1,9 @@
-function scrollToLink() {
-  const links = document.querySelectorAll(".toc a");
-  links.forEach((link) => {
-    link.onclick = () => {
-      window.scrollTo(
-        0,
-        document.querySelector(link.getAttribute("href")).offsetTop
-      );
-    };
-  });
-}
-
-function initSidebar() {
-  scrollToLink();
-
+function toggleSidebarNav() {
   // toggle sidebar nav and panel
+  const activeTabClass = "sidebar-nav-active";
+  const activePanelClass = "sidebar-panel-active";
   document.querySelectorAll(".sidebar-nav li").forEach((el) => {
     el.onclick = function() {
-      const activeTabClass = "sidebar-nav-active";
-      const activePanelClass = "sidebar-panel-active";
       if (this.classList.contains(activeTabClass)) return;
       document
         .querySelector("." + activePanelClass)
@@ -31,64 +17,82 @@ function initSidebar() {
       this.classList.add(activeTabClass);
     };
   });
-  /**
-   * display TOC
-   */
-  // main of scroll
-  if (document.querySelectorAll(".toc-link").length) {
-    let tocList = document
-      .querySelector(".post-content")
-      .querySelectorAll("h1, h2, h3, h4, h5, h6");
+}
 
-    window.addEventListener("scroll", function() {
-      findHeadPosition(tocList, window.scrollY);
+function listennSidebarTOC() {
+  const navItems = document.querySelectorAll(".post-toc li");
+  const sections = [...navItems].map((element) => {
+    const link = element.querySelector(".toc-link");
+    const target = document.querySelector(link.getAttribute("href"));
+    link.addEventListener("click", (event) => {
+      event.preventDefault();
+      window.scrollTo(0, target.offsetTop + 1);
     });
-  }
+    return target;
+  });
 
-  function updateAnchor(anchor) {
-    if (window.history.replaceState && anchor !== window.location.hash) {
-      window.history.replaceState(undefined, undefined, anchor);
+  function activateNavByIndex(target) {
+    if (target.classList.contains("active-current")) return;
+
+    document.querySelectorAll(".post-toc .active").forEach((element) => {
+      element.classList.remove("active", "active-current");
+    });
+    target.classList.add("active", "active-current");
+    let parent = target.parentNode;
+    while (!parent.matches(".post-toc")) {
+      if (parent.matches("li")) parent.classList.add("active");
+      parent = parent.parentNode;
     }
   }
 
-  function activeLink(link) {
-    document.querySelectorAll(".toc-item").forEach((item) => {
-      item.classList.remove("active");
-    });
-    let linkParent = link.parentNode;
-    linkParent.classList.add("active");
-    while (linkParent.parentNode.classList.contains("toc-child")) {
-      linkParent = linkParent.parentNode.parentNode;
-      linkParent.classList.add("active");
+  function findIndex(entries) {
+    let index = 0;
+    let entry = entries[index];
+    if (entry.boundingClientRect.top > 0) {
+      index = sections.indexOf(entry.target);
+      return index === 0 ? 0 : index - 1;
     }
-  }
-
-  // add active class for cur toc
-  // DOM Hierarchy:
-  // ol.toc > (li.toc-item, ...)
-  // li.toc-item > (a.toc-link, ol.toc-child > (li.toc-item, ...))
-  function findHeadPosition(tocList, top) {
-    let curId = "";
-    for (let i = 0; i < tocList.length; i++) {
-      const el = tocList[i];
-      if (top > el.offsetTop - 64) {
-        curId = "#" + el.attributes.id.value;
+    for (; index < entries.length; index++) {
+      if (entries[index].boundingClientRect.top <= 0) {
+        entry = entries[index];
       } else {
-        break;
+        return sections.indexOf(entry.target);
       }
     }
-
-    let curActiveLink = document.querySelector(".toc-link.active");
-
-    if (!curId) curId = "#" + document.querySelector("h2").id;
-    if (!curActiveLink || curActiveLink.attributes.href !== curId) {
-      let curLink = document.querySelector(".toc-link[href='" + curId + "']");
-      if (curLink) {
-        activeLink(curLink);
-        updateAnchor(curId);
-      }
-    }
+    return sections.indexOf(entry.target);
   }
+
+  function createIntersectionObserver(marginTop) {
+    console.log(marginTop);
+    marginTop = Math.floor(marginTop + 10000);
+    console.log(marginTop);
+    let intersectionObserver = new IntersectionObserver(
+      (entries, observe) => {
+        let scrollHeight = document.documentElement.scrollHeight + 100;
+        if (scrollHeight > marginTop) {
+          observe.disconnect();
+          createIntersectionObserver(scrollHeight);
+          return;
+        }
+        let index = findIndex(entries);
+        activateNavByIndex(navItems[index]);
+      },
+      {
+        rootMargin: marginTop + "px 0px -100% 0px",
+        threshold: 0,
+      }
+    );
+    sections.forEach((element) => {
+      element && intersectionObserver.observe(element);
+    });
+  }
+
+  createIntersectionObserver(document.documentElement.scrollHeight);
+}
+
+function initSidebar() {
+  toggleSidebarNav();
+  listennSidebarTOC();
 }
 document.addEventListener("DOMContentLoaded", initSidebar);
 document.addEventListener("pjax:success", initSidebar);
