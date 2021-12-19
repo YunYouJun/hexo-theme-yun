d37f8ec2518ef9368f474e1e66e1ddeab01adc04
YunYouJun <me@yunyoujun.cn>
2021-12-19 20:35:40 +0800

    feat: add pwa for docs

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -1,17 +1,23 @@
 import path from "path";
 import Components from "unplugin-vue-components/vite";
+import { VitePWA } from "vite-plugin-pwa";
+
 import { defaultConfig } from "./node_modules/vitepress-theme-you/src/config";
 
 import type { UserConfig } from "vite";
 
-defaultConfig.plugins.push(
+defaultConfig.plugins = defaultConfig.plugins.concat([
   Components({
     dirs: [path.resolve(__dirname, ".vitepress/theme/components")],
     extensions: ["vue", "ts"],
     include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
     dts: true,
-  })
-);
+  }),
+
+  VitePWA({
+    registerType: "autoUpdate",
+  }),
+]);
 
 const viteConfig: UserConfig = {
   resolve: {
