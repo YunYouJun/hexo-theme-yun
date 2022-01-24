8779e6103128f325702a45676e4154b4f19c055b
YunYouJun <me@yunyoujun.cn>
2022-01-24 18:21:21 +0800

    fix: waline load module

--- scripts/events/config.js
+++ scripts/events/config.js
@@ -1,4 +1,4 @@
-const { merge } = require("./utils");
+const { merge } = require('./utils')
 
 /**
  * 合并语言
@@ -6,41 +6,30 @@ const { merge } = require("./utils");
  * @param {*} languages
  */
 function mergeLanguages(hexo, languages) {
-  const { language } = hexo.config;
-  const { i18n } = hexo.theme;
+  const { language } = hexo.config
+  const { i18n } = hexo.theme
 
   const mergeLang = (lang) => {
-    i18n.set(lang, merge(i18n.get([lang]), languages[lang]));
-  };
+    i18n.set(lang, merge(i18n.get([lang]), languages[lang]))
+  }
 
   if (Array.isArray(language)) {
-    for (let lang of language) {
-      mergeLang(lang);
-    }
-  } else {
-    mergeLang(language);
+    for (const lang of language)
+      mergeLang(lang)
+  }
+  else {
+    mergeLang(language)
   }
 }
 
 module.exports = (hexo) => {
-  const data = hexo.locals.get("data");
-
-  /**
-   * Merge configs from _data/yun.yml into hexo.theme.config.
-   */
-  if (data.yun) {
-    merge(hexo.config, data.yun);
-    merge(hexo.theme.config, data.yun);
-    // hexo auto merge theme.config & config.theme_config
-  }
+  const data = hexo.locals.get('data')
 
   // config for test
-  if (data.test && process.env.NODE_ENV === "test") {
-    merge(hexo.theme.config, data.test);
-  }
+  if (data.test && process.env.NODE_ENV === 'test')
+    merge(hexo.theme.config, data.test)
 
   // merge languages
-  if (data.languages) {
-    mergeLanguages(hexo, data.languages);
-  }
-};
+  if (data.languages)
+    mergeLanguages(hexo, data.languages)
+}
