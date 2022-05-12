236898ce083baf6dab1e3a9736819ea81791a6b7
YunYouJun <me@yunyoujun.cn>
2022-05-12 22:19:47 +0800

    ci: add scripts for publish & release

---
+++ scripts/release.mjs
@@ -0,0 +1,13 @@
+import { $, fs } from 'zx'
+import { execSync } from 'child_process'
+
+const yunPkg = 'packages/hexo-theme-yun/package.json'
+execSync(`npx bumpp ${yunPkg}`, { stdio: 'inherit' })
+
+const { version } = await fs.readJSON(yunPkg)
+
+await $`git add .`
+await $`git commit -m "chore: release v${version}"`
+await $`git tag v${version}`
+await $`git push`
+await $`git push origin --tags`
